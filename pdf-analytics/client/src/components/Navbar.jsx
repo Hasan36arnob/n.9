e7107b70a80cd4d6b62df10397934e2cddb418e8
Hasan36arnob <arnobhasanice@gmail.com>
2026-03-17 08:10:29 +0600

    21

--- pdf-analytics/client/src/components/Navbar.jsx
+++ pdf-analytics/client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+//this is the navbar comp
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
